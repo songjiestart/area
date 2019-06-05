b323bf10f92badd37bdf97dc45bc5174242e9451
songjie <zhaisongjie@benmu_health.com>
2019-06-05 10:59:57 +0800

    省市sql

---
+++ module1/src/main/java/Hello.java
@@ -0,0 +1,23 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * @Author: songjie
+ * @Date: 2019-04-03 10:20
+ * @Description:
+ */
+public class Hello implements Serializable {
+    private String nam1;
+    private String nam2;
+    private String nam3;
+
+    public static void main(String[] args) {
+        int n = 5;
+
+
+    }
+
+    public static int get(int i) {
+        return 123;
+    }
+}
